f66ac4cfc93ecb8de6f6bfb2f9c70091c7675f21
Emanuele Bertuol <emanuelebertuol@gmail.com>
2021-07-29 03:14:43 +0200

    Update index.js

--- src/mybet21/index.js
+++ src/mybet21/index.js
@@ -46,7 +46,8 @@ router.get("/oddsmatcher", async (req, res) => {
         alt: "media",
       })
       .then((response) => response.data);
-
+      
+    odds = odds.slice(0, 99)
     //console.log(odds)
     console.log(odds.length)
     res.status(200).send(odds);
